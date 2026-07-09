2f633ba11ae061d1536f3377d45ae5f8418c4a9e
Praveen Sonesha <praveensonesha2003@gmail.com>
2026-07-09 10:53:22 +0530

    Update 01_install_chromium_deps.sh

--- .platform/hooks/prebuild/01_install_chromium_deps.sh
+++ .platform/hooks/prebuild/01_install_chromium_deps.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 sudo dnf install -y atk cups-libs gtk3 libXcomposite libXcursor libXdamage \
   libXext libXi libXrandr libXScrnSaver libXtst pango alsa-lib nss at-spi2-atk
+
+# Native build dependencies for the `canvas` package (used by chartjs-node-canvas)
+sudo dnf install -y gcc-c++ make cairo-devel pango-devel libjpeg-turbo-devel \
+  giflib-devel librsvg2-devel pixman-devel pkgconf-pkg-config
